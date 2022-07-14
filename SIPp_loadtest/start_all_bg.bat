a01cf7df6530a6ddd04dafa0b1ea20f77a8dbbac
Ayyanar181195 <90748202+Ayyanar181195@users.noreply.github.com>
2022-07-14 13:46:12 +0530

    Delete SIPp_loadtest directory

--- SIPp_loadtest/start_all_bg.bat
+++
@@ -1,9 +0,0 @@
-del *.log
-"C:\Program Files\Sipp_3.1\sipp.exe" -sf mcp_sim.xml -p 5071 -trace_err -bg
-timeout /t 1
-"C:\Program Files\Sipp_3.1\sipp.exe" -sf mcp_sim.xml -p 5072 -trace_err -bg
-timeout /t 1
-"C:\Program Files\Sipp_3.1\sipp.exe" -sf mcp_sim.xml -p 5073 -trace_err -bg
-timeout /t 1
-"C:\Program Files\Sipp_3.1\sipp.exe" -sf mcp_sim.xml -p 5074 -trace_err -bg
-cmd
