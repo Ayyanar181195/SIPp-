8af1aa08ac5cf6af5e8576f83b0fb0fe61a9855b
Ayyanar181195 <90748202+Ayyanar181195@users.noreply.github.com>
2022-07-14 13:38:41 +0530

    Add files via upload

---
+++ sipp-3.3/ctic_perf/NGI04_caller.bat
@@ -0,0 +1,1 @@
+"C:\Progra~1\Sipp_3.1\sipp" -sf "C:\Progra~1\sipp_execute_perf\ngi04_caller.xml" 172.24.133.48 -m 4320000 -r 25 -l 999999999999 -p 11200 -s 9090 -trace_stat -trace_err -trace_rtt -trace_screen 
